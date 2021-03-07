519116beb3b0a0e2b973d9a84b3995ec0b4119d9
Joanne Chen <joanne_r_chen@yahoo.com>
2021-03-07 15:45:04 +0800

    decoded converted to utf-8, then html markdown and now readable format

--- src/components/RepoDetails.js
+++ src/components/RepoDetails.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import moment from 'moment';
-import { Link } from 'react-router-dom';
+import { Buffer } from 'buffer/';
+import marked from 'marked';
 // styling
 import Card from 'react-bootstrap/Card';
 import axios from 'axios';
@@ -11,22 +12,23 @@ export const RepoDetails = (details) => {
 
     const owner = repoDetails.owner.login;
     const repoName = repoDetails.name;
-    console.log(owner)
-    console.log(repoName)
     const [readme, setReadme] = useState("")
 
     useEffect(() => {
         const fetchReadme = async () => {
             const res = await axios(`http://api.github.com/repos/${owner}/${repoName}/contents/README.md`)
-            console.log(res.data.content)
-            setReadme(res.data.content)
+            console.log(res.data.content);
+
+            const buff = Buffer.from(res.data.content, 'base64').toString('utf-8');
+            console.log(buff);
+            setReadme(marked(buff));
 
         }
         fetchReadme();
     }, [])
 
     return (
-        <div>
+        <div style={{padding: "2rem"}}>
             <Card className="text-center">
                 <Card.Header>
                     <Card.Link href={repoDetails.html_url} target="_blank">Link to the repo</Card.Link>
@@ -34,18 +36,22 @@ export const RepoDetails = (details) => {
                 <Card.Body>
                     <Card.Subtitle style={{color: 'grey'}}>Created on {moment(repoDetails.created_at).format('MMMM do YYYY')}</Card.Subtitle>
                     <br></br>
-                    <Card.Title>{repoDetails.name}</Card.Title>
+                    <Card.Subtitle style={{color: 'grey'}}>Updated on {moment(repoDetails.updated_on).format('MMMM do YYYY')}</Card.Subtitle>
                     <br></br>
-                    <Card.Subtitle style={{color: 'grey'}}>Owner: {repoDetails.owner.login}</Card.Subtitle>
+                    <Card.Title as="h2">{repoDetails.name}</Card.Title>
+                    <br></br>
+                    <Card.Subtitle as="h5" style={{color: 'grey'}}>Owner: {repoDetails.owner.login}</Card.Subtitle>
                     <br></br>
                     <Card.Subtitle style={{color: 'grey'}}>Language: {repoDetails.language}</Card.Subtitle>
                     <br></br>
                     <Card.Text>{repoDetails.description}</Card.Text>
                 </Card.Body>
- 
                 <Card.Footer style={{color: 'grey'}}>Stars: {repoDetails.stargazers_count}  ||  Open issues: {repoDetails.open_issues_count}  ||  Forks: {repoDetails.forks_count}  ||  Watchers: {repoDetails.watchers_count}</Card.Footer>
+
+                <Card.Footer dangerouslySetInnerHTML={{__html: readme}}className="text-left" style={{color: 'grey'}}></Card.Footer>
+                
+
             </Card>
-            <p>{readme}</p>
         </div>
     )
 }
